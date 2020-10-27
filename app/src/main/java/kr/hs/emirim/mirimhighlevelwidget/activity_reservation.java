f6c23c55908a88b150b409d5da1cde0408c1134b
dwrdd <s2019w30@e-mirim.hs.kr>
2020-10-27 20:23:07 +0900

    Complet activity_reservation.xml

---
+++ app/src/main/java/kr/hs/emirim/mirimhighlevelwidget/activity_reservation.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.mirimhighlevelwidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_reservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
